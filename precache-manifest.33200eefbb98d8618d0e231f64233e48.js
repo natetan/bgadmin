e28a468c87d5f623a18871493a74580016b38c3f
Nate Tan <nytan67@gmail.com>
2020-02-12 18:07:24 -0800

    Updates

---
+++ precache-manifest.33200eefbb98d8618d0e231f64233e48.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b19a8b5c631cc14cfd3a567e37772e98",
+    "url": "/bgadmin/index.html"
+  },
+  {
+    "revision": "2ea29b1dad101e93b768",
+    "url": "/bgadmin/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "3660339aa3ec5d6ca31a",
+    "url": "/bgadmin/static/css/main.eebcef02.chunk.css"
+  },
+  {
+    "revision": "2ea29b1dad101e93b768",
+    "url": "/bgadmin/static/js/2.c810270d.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/bgadmin/static/js/2.c810270d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3660339aa3ec5d6ca31a",
+    "url": "/bgadmin/static/js/main.17d76c24.chunk.js"
+  },
+  {
+    "revision": "d56e144f87fa16fab5cd",
+    "url": "/bgadmin/static/js/runtime-main.f5fc3079.js"
+  },
+  {
+    "revision": "11ec39f3817246652ba280f6c8d918a5",
+    "url": "/bgadmin/static/media/bamboo-bg-1.11ec39f3.jpg"
+  },
+  {
+    "revision": "ff9492c244055ff630a887617d110484",
+    "url": "/bgadmin/static/media/bamboo-bg-2.ff9492c2.jpg"
+  },
+  {
+    "revision": "737cc8d6bf4171ec07ae039ff54edcd4",
+    "url": "/bgadmin/static/media/bg-front.737cc8d6.jpg"
+  }
+]);
