02a565b0e71fe557e34ac787a0aadd2ddf2ee1b3
Nate Tan <nytan67@gmail.com>
2020-02-13 01:22:52 -0800

    Modify menu and change design

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -9,42 +9,78 @@ import './Menu.scss';
 import menu from '../../resources/menu.json';
 
 const Menu = () => {
-  const getItems = type => {
-    let items = menu;
-    // if (type === 'odd') {
-    //   items = menu.filter(i => {
-    //     return i.id % 2 === 1;
-    //   });
-    // } else if (type === 'even') {
-    //   items = menu.filter(i => {
-    //     return i.id % 2 === 0;
-    //   });
-    // }
+  let categories = {};
+  menu.forEach(i => {
+    let cat = i.category;
+    if (!categories[cat]) {
+      categories[cat] = [];
+    }
+    categories[cat].push(i);
+  })
+  const doSomething = () => {
+    let items = Object.keys(categories).map((c, index) => {
+      let itemsInCategory = categories[c].map(item => {
+        return (
+          <Row key={`item-${item.id}`}>
+            <Col lg='1'>
+              <span className='item-name'>{`${item.id}. `}</span>
+            </Col>
+            <Col lg='3'>
+              <span className='item-name'>{`${item.chinese}`}</span>
+            </Col>
+            <Col lg='7'>
+              <span className='item-name'>{item.name}</span>
+            </Col>
+            <Col className='item-name-price' lg='1'>
+              <span>{item.price.small ? item.price.large : item.price}</span>
+            </Col>
+          </Row>
+        )
+      })
+      return (
+        <div key={`category-${index}`} className='category-section'>
+          <h2>{c}</h2>
+          {itemsInCategory}
+        </div>
+      )
+    });
+    return items;
+  }
+  const getItems = () => {
     let res = menu.map(item => {
       let price = item.price.small ? `S: $${item.price.small} L: $${item.price.large}` : `$${item.price}`;
-      console.log(item.price);
       let data = {
         id: item.id,
         name: item.name,
         price: price
       }
       return (
-        <Col md='4' key={data.id}>
-          <MenuItem data={data} />
-        </Col>
+        <Row key={data.id}>
+          <Col lg='1'>
+            <span className='item-name'>{`${item.id}. `}</span>
+          </Col>
+          <Col lg='3'>
+            <span className='item-name'>{`${item.chinese}`}</span>
+          </Col>
+          <Col lg='7'>
+            <span className='item-name'>{item.name}</span>
+          </Col>
+          <Col className='item-name-price' lg='1'>
+            <span>{item.price.small ? item.price.large : item.price}</span>
+          </Col>
+        </Row>
       )
     });
     return res;
   }
   let items = getItems();
+  let test = doSomething();
   return (
     <div className='menu'>
       <h1>Menu</h1>
       <p>See what delicious meals we have to offer!</p>
       <Container>
-        <Row>
-          {items}
-        </Row>
+        {test}
       </Container>
     </div>
   )
