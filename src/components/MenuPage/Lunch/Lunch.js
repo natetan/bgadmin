bbffa47ea26a395d73366d9183fa7bcaa6d0cee3
Nate Tan <nytan67@gmail.com>
2020-02-14 15:23:18 -0800

    Add combos to lunch menu

--- src/components/MenuPage/Lunch/Lunch.js
+++ src/components/MenuPage/Lunch/Lunch.js
@@ -1,12 +1,40 @@
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
 import React from 'react';
-
-import './Lunch.scss';
+import Row from 'react-bootstrap/Row';
 
 import Menu from '../Menu/Menu';
+import MenuItem from '../MenuItem/MenuItem';
+
+import './Lunch.scss';
+import combos from '../../../resources/combos.json';
 import lunch from '../../../resources/lunch.json';
 
 const Lunch = () => {
-  return <Menu menu={lunch} type='lunch' />
+  let lunchCombos = combos.lunch.map((c, i) => {
+    let data = {
+      name: c.name, 
+      items: c.items,
+      price: c.price
+    };
+    return (
+      <Col key={i} md='6' lg='4'>
+        <MenuItem data={data} />
+      </Col>
+    )
+  })
+  return (
+    <div className='lunch'>
+      <Container>
+        <h1>Combos</h1>
+        <Row>
+          {lunchCombos}
+        </Row>
+      </Container>
+      <Menu menu={lunch} type='lunch' />
+    </div>
+    
+  );
 }
 
 export default Lunch;
--- src/components/MenuPage/MenuItem/MenuItem.js
+++ src/components/MenuPage/MenuItem/MenuItem.js
@@ -1,5 +1,9 @@
 import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
 import React from 'react';
+import Row from 'react-bootstrap/Row';
+
+import { toMoneyDisplay } from '../../../lib/util/displayUtils';
 
 import './MenuItem.scss';
 
@@ -15,23 +19,27 @@ const MenuItem = props => {
       </Card.Body>
     );
   } else {
+    let items = props.data.items.map((item, index) => {
+      return <Row key={`combo-item-${index}`}>
+        <Col md='12'>
+          <Card.Text>{item}</Card.Text>
+        </Col>
+      </Row>
+    });
     content = (
       <>
-        <Card.Header>{props.data.id}</Card.Header>
+        <Card.Header>{props.data.name}</Card.Header>
         <Card.Body>
-          <Card.Title>{props.data.name}</Card.Title>
-          <Card.Img src={props.src} />
-          <Card.Text>
-            {props.name}
-          </Card.Text>
+          {/* <Card.Title>{props.data.name}</Card.Title> */}
+          {items}
         </Card.Body>
-        <Card.Footer className='text-muted'>{props.name}</Card.Footer>
+        <Card.Footer className='text-muted'>{toMoneyDisplay(props.data.price)}</Card.Footer>
       </>
     )
   }
   return (
     <div className='menu-item'>
-      <Card className='box-shadow'>
+      <Card className='box-shadow combo'>
         {content}
       </Card>
     </div>
